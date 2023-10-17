543af02cc607a45abc5c55539fbe3995cd00b501
joppe montezinos <joppe.montezinos@gmail.com>
2023-10-17 11:08:13 +0200

    finito

--- frontend/components/Recommendations.vue
+++ frontend/components/Recommendations.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="recommendation-container">
     <h2>Recommended Anime</h2>
-    <div v-for="anime in recommendedAnime" :key="anime.name">
-      <div class="recommendation">
+    <div class="anime-list">
+      <div v-for="anime in recommendedAnime" :key="anime.name" class="anime-item">
         <h3>
           <a :href="anime.url">{{ anime.name }}</a>
         </h3>
@@ -15,3 +15,57 @@
 <script setup>
 const props = defineProps(['recommendedAnime']);
 </script>
+
+<style scoped lang="scss">
+.recommendation-container {
+  font-family: 'Arial', sans-serif;
+  max-width: 700px;
+  margin: 50px auto;
+  padding: 20px;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+
+  h2 {
+    color: #2c3e50;
+    margin-bottom: 20px;
+    text-align: center;
+    border-bottom: 2px solid #e74c3c;
+    padding-bottom: 15px;
+  }
+
+  .anime-list {
+    margin-top: 20px;
+
+    .anime-item {
+      margin-bottom: 20px;
+      padding: 15px;
+      border-radius: 6px;
+      background-color: #f9f9f9;
+      transition: background-color 0.3s ease;
+
+      &:hover {
+        background-color: #e9e9e9;
+      }
+
+      h3 {
+        margin: 0;
+        margin-bottom: 10px;
+        a {
+          color: #2980b9;
+          text-decoration: none;
+          transition: color 0.3s ease;
+
+          &:hover {
+            color: #e74c3c;
+          }
+        }
+      }
+
+      p {
+        margin: 0;
+        color: #7f8c8d;
+      }
+    }
+  }
+}
+</style>
